4eefd3b66b4a6cea82cbbfe509124fb6d2e3116b
hnnthecore <rapa.n@live.it>
2024-11-19 20:28:40 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Welcome to my Homepage</h1>
+      <h4>It's swiss Kumar</h4>
+    </>
+  )
+}
+
+export default App
